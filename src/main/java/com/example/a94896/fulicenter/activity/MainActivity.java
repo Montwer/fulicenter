5d28db2a8a3d2741a5edd7784f4d9ccc8fb861fa
LSL <948961071@qq.com>
2016-10-14 15:44:42 +0800

    添加Log工具类并在MainActivity测试

--- src/main/java/com/example/a94896/fulicenter/activity/MainActivity.java
+++ src/main/java/com/example/a94896/fulicenter/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.a94896.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.a94896.fulicenter.R;
+import com.example.a94896.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("MainActivity onCreate");
     }
 }
