d1f1d1acdd03fc378f451b941e0ab73535a6a3e8
LSL <94891071@qq.com>
2016-10-24 19:18:21 +0800

    添加SharePrefrenceUtils保存用户名

--- src/main/java/com/example/a94896/fulicenter/activity/SplashMainActivity.java
+++ src/main/java/com/example/a94896/fulicenter/activity/SplashMainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.a94896.fulicenter.Dao.SharePrefrenceUtils;
 import com.example.a94896.fulicenter.Dao.UserDao;
 import com.example.a94896.fulicenter.FuLiCenterApplication;
 import com.example.a94896.fulicenter.R;
@@ -13,7 +14,7 @@ import com.example.a94896.fulicenter.utils.MFGT;
 
 public class SplashMainActivity extends AppCompatActivity {
     private static final String TAG=SplashMainActivity.class.getSimpleName();
-private final long sleepTime=2000;
+    private final long sleepTime=2000;
     SplashMainActivity mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +28,16 @@ private final long sleepTime=2000;
             @Override
             public void run() {
                User user = FuLiCenterApplication.getUser();
-                L.e(TAG," fulicenter,user="+user);
-                if (user==null) {
+                L.e(TAG," FuLiCenter,user="+user);
+                String username= SharePrefrenceUtils.getInstence(mContext).getUser();
+                L.e(TAG,"fulicenter,username="+username);
+                if (user==null&&username!=null) {
                     UserDao dao = new UserDao(mContext);
-                    dao.getUser("a9527010");
+                    user=dao.getUser(username);
                     L.e(TAG,"database,user="+user);
+                    if (user!=null){
+                        FuLiCenterApplication.setUser(user);
+                    }
                 }
                 MFGT.gotoMainActivity(SplashMainActivity.this);
                 finish();
